0c0860075d46b1dd26c6fc3ece9db49049a0701d
ElectroShepard <ElectroShepard@users.noreply.github.com>
2020-11-24 23:03:12 +0100

    News

--- src/Map.java
+++ src/Map.java
@@ -1,27 +1,27 @@
+//import java.util.ArrayList;
+
 import java.util.ArrayList;
 
 public class Map<K, V> {
-    private ArrayList<HashNode<K,V>> hexArray;
+    private ArrayList<HashNode<K, V>> hexArray;
     private int hexArrayCapacity;
     private int size;
 
     public Map() {
-        hexArray = new Arraylist<>();
+        hexArray = new ArrayList<>();
         hexArrayCapacity = 10;
         size = 0;
 
-        for(int i =0; i < hexArrayCapacity; i++)
-        {
+        for (int i = 0; i < hexArrayCapacity; i++) {
             hexArray.add(null);
         }
     }
 
-    public int getSize()
-    {
+    public int getSize() {
         return size;
     }
-    public boolean isEmpty()
-    {
+
+    public boolean isEmpty() {
         return (getSize() == 0);
     }
 
@@ -30,61 +30,52 @@ public class Map<K, V> {
         int index = hashcode % hexArrayCapacity;
         return index;
     }
-    public V removeKey (K key)
-    {
+
+    public V removeKey(K key) {
         int index = getIndex(key);
-        HashNode<K,V> head = hexArray.get(index);
-        HashNode<K,V> prev = null;
-        while(head != null)
-        {
-            if(head.key.equals(key))
-            {
+        HashNode<K, V> head = hexArray.get(index);
+        HashNode<K, V> prev = null;
+        while (head != null) {
+            if (head.key.equals(key)) {
                 break;
             }
             prev = head;
             head = head.next;
         }
-        if(head == null)
-        {
+        if (head == null) {
             return null;
         }
         size--;
-        if(prev!=null)
-            prev.next=head.next;
+        if (prev != null)
+            prev.next = head.next;
         else {
-            hexArray.set(index,head.next);
+            hexArray.set(index, head.next);
         }
         return head.value;
     }
-    public V getValue(K key)
-    {
-        int index = getIndex(key);
-        HashNode<K,V> head = hexArray.get(index);
 
-        while(head != null) {
-            if(head.key.equals(key))
+    public V getValue(K key) {
+        int index = getIndex(key);
+        HashNode<K, V> head = hexArray.get(index);
+
+        while (head != null) {
+            if (head.key.equals(key))
                 return head.value;
             head = head.next;
         }
         return null;
     }
-    public void addKeyValue(K key, V value)
-    {
-        int index = getIndex(key);
-        HashNode<K,V> head = hexArray.get(index);
 
-        while(head==null)
+    public void addKeyValue(K key, V value) {
+        int index = getIndex(key);
+        HashNode<K, V> head = hexArray.get(index);
+
+        while (head == null)
         {
-            if (head.key.equals(key))
-            {
+            if (head.key.equals(key)) {
                 head.value = value;
             }
-
         }
 
-
-    public String getValue(String  identifier) {
-
-        return identifier;
     }
 }
