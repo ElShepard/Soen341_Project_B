a7faa5bbe67645a6f28a187bf015557ea3e37d60
ElectroShepard <ElectroShepard@users.noreply.github.com>
2020-11-24 22:47:15 +0100

    News

--- src/Map.java
+++ src/Map.java
@@ -6,6 +6,31 @@ public class Map<K, V> {
     private int size;
 
     public Map() {
+        hexArray = new Arraylist<>();
+        hexArrayCapacity = 10;
+        size = 0;
 
+        for(int i =0; i < hexArrayCapacity; i++)
+        {
+            hexArray.add(null);
+        }
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+    public boolean isEmpty()
+    {
+        return (getSize() == 0);
+    }
+    private getIndex(K key) {
+        int hashcode = key.hashCode();
+        int index = hashcode % hexArrayCapacity;
+        return
+    }
+    public String getValue(String  identifier) {
+
+        return identifier;
     }
 }
